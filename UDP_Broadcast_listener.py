aed76c028788dcbc440c3f1b743fe0174c52872b
Mark Moore <mark@moore.dk>
2026-05-07 13:29:42 +0200

    Udp broadcast/listen now works on a windows machine with multiple NICs - Just choose which address to send from and listen on

--- UDP_Broadcast_listener.py
+++ UDP_Broadcast_listener.py
@@ -1,19 +1,42 @@
 import socket
 from IPy import IP
 
-def listen():
-    hostname = socket.gethostname()
-    IPAddr = socket.gethostbyname(hostname)
+#Asks user to select an IP, address as source for broadcasting.
 
-    print("Your Computer Name is:" + hostname)
-    print("Your Computer IP Address is:" + IPAddr)
+def selectInterface():
+    interfaces = socket.getaddrinfo(host=socket.gethostname(), port=None, family=socket.AF_INET)
+    allips = [ip[-1][0] for ip in interfaces]
+
+    print("Here's all the LAN IPs for this computer:")
+    i=0
+    for ip in allips:
+        print(f"{i}: {ip}")
+        i+=1
+    choice = int(input("Which one would you like to use to transmit? "))
+    interface = allips[choice]
+
+    #interface = interface[0]
+    #print(f"chose {interface}")
+    # create a socket object
+
+    return interface
+
+
+def listen(IPAddr):
+    #hostname = socket.gethostname()
+    #IPAddr = socket.gethostbyname(hostname)
+
+    #print("Your Computer IP Address is:" + IPAddr)
 
     # create a socket object
     listener = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
+    listener.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
     #TX_port = 8001
     RX_port = 8002
     listen_addr = ("", RX_port)
+
     listener.bind(listen_addr)
 
     print(f"Listening on port {RX_port}")
@@ -26,5 +49,5 @@ def listen():
 
         print(f"Received {received} from {remoteIP}")
 
-
-listen()
+addr = selectInterface()
+listen(addr)
--- UDP_Broadcaster.py
+++ UDP_Broadcaster.py
@@ -1,28 +1,45 @@
 import socket
 
-def transmit():
+#Asks user to select an IP address to listen for broadcasted messages.
 
+def selectInterface():
     interfaces = socket.getaddrinfo(host=socket.gethostname(), port=None, family=socket.AF_INET)
     allips = [ip[-1][0] for ip in interfaces]
 
+    print("Here's all the LAN IPs for this computer:")
+    i=0
+    for ip in allips:
+        print(f"{i}: {ip}")
+        i+=1
+    choice = int(input("Which one would you like to use to transmit? "))
+    interface = allips[choice]
 
+    #interface = interface[0]
+    #print(f"chose {interface}")
     # create a socket object
+
+    return interface
+
+
+
+def getSocket(sourceIP):
     transmitter = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
+    #print(broadcast_addr)
+    transmitter.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    transmitter.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    transmitter.bind((sourceIP, 0)) #open an outgoing port on chosen interface. Don't care about port number :)
+    return transmitter
 
-    broadast_Port = 8002
-
-    broadcast_addr = (socket.inet_aton('255.255.255.255'),broadast_Port)
-
-    transmitter.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1023)
-
-    transmitter.bind((allips[0], 0)) #open an outgoing port on first interface. Don't care about port number :)
-
-    print(f"Read to TX on {broadcast_addr}")
-
-    while True:
-
-        tx = input("What to send?").strip().encode()  # convert string to bytes
-        transmitter.sendto(tx,broadcast_addr)
+def transmit(msg, conn, dest):
+        conn.sendto(tx,dest)
 
 
-transmit()
+broadcast_Port = 8002
+sourceIP = selectInterface()
+broadcast_tuple = ('<broadcast>',broadcast_Port)
+transmitter = getSocket(sourceIP)
+print(f"Ready to TX from {sourceIP} to port {broadcast_Port}")
+
+while True:
+    tx = input("What to send?").strip().encode()  # convert string to bytes
+    transmit(tx,transmitter,broadcast_tuple)
